6b4e38a2cf03b0111007fa334527d53edaac116a
tae <taenaive@gmail.com>
2024-07-28 15:27:46 -0400

    new version 1.0.1

--- src/async/pollManager.ts
+++ src/async/pollManager.ts
@@ -5,7 +5,7 @@ const GlobalRetryLimit = 3;
 
 export async function poll(fn: (...args: any ) => Promise<any>, ...args:any): Promise<void> {
     for (let i = 0; i < GlobalRetryLimit; i++) {
-         console.log(`Waiting ${i}...`);
+        //  console.log(`Waiting ${i}...`);
         await sleep(1000);
         await fn(...args)
     }
